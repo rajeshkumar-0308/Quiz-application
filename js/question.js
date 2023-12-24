3176a0193a185fbbaa2adfecdebd1dc730582468
unknown <rajeshsaravanan2004@gmail.com>
2023-12-24 22:11:18 +0530

    1st commit

---
+++ js/question.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the first Prime Minister of India?",
+    answer: "Jawaharlal Nehru",
+    options: [
+      "P.J. Abdul Kalaam",
+      "Jawaharlal Nehru",
+      "Indira Gandhi",
+      "Narendra Modi"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which is the biggest continent?",
+    answer: "Asia",
+    options: [
+      "Asia",
+      "Africa",
+      " Australia",
+      "Europe"
+    ]
+  },
+    {
+    numb: 3,
+    question: " Which planet is known as “The Blue Planet”?",
+    answer: "Earth",
+    options: [
+      " Mercury",
+      "Venus",
+      "Uranus",
+      "Earth"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the full form of RBI?",
+    answer: "Reserve Bank of India",
+    options: [
+      "Reputed Bank of India",
+      "Reserve Bank of India",
+      "Recovery Bank of India",
+      "Reduced Bank of India"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many states are there in India?",
+    answer: "29",
+    options: [
+      "27",
+      "28",
+      "29",
+      "30"
+    ]
+  },
+  
+];
